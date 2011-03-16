b22b7ca36bfee7d035423f6a1fd2595a1dc8a3ed
Jesse Domenget <jddomenget@gmail.com>
2011-03-15 22:16:08 -0500

    Initial GUI Screens

---
+++ src/GUI/RunMain.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package GUI;
+import GUI.*;
+
+/**
+ *
+ * @author talkingjd72
+ */
+public class RunMain {
+
+    public static void main(String args[]){
+        //AcceptScreen as = new AcceptScreen();
+        ServerMode sm = new ServerMode();
+        //GameMode gm = new GameMode();
+
+        //as.main(null);
+        sm.main(null);
+        //gm.main(null);
+    }
+}
